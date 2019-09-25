059c9002f90be662a7a50b6da32d90d0e2fb60cd
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2019-09-25 18:41:01 +0200

    add tw

--- src/components/SpeakerInfo/index.js
+++ src/components/SpeakerInfo/index.js
@@ -5,7 +5,11 @@ import './index.scss'
 const SpeakerInfo = ({ stage, show }) =>
   stage.speaker && (
     <div className={classnames('speaker-info', show && 'show-speaker-info')}>
-      <span className="name">{stage.speaker.name}</span>
+      <span className="name">{stage.speaker.name}
+        <br />
+        <span className="twitter">{stage.speaker.twitter}</span>
+      </span>
+
       <span className="topic">{stage.speaker.topic}</span>
     </div>
   )
